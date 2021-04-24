07b56fbbbf5f092baedfbf801a1cabd921215bac
Thomas Rinsma <thomasrinsma@gmail.com>
2021-04-24 16:56:44 +0200

    Added pokered badges

--- src/games/pokemon_red.cc
+++ src/games/pokemon_red.cc
@@ -1,5 +1,9 @@
 #include "games.h"
 
+
+// https://datacrystal.romhacking.net/wiki/Pok%C3%A9mon_Red/Blue:RAM_map
+
+
 // Read at most `length` poke-encoded characters from `addr`
 std::string getPokeStringAt(Dromaius *emu, uint16_t addr, uint16_t len) {
 	std::string str;
@@ -27,77 +31,97 @@ std::string getPokeStringAt(Dromaius *emu, uint16_t addr, uint16_t len) {
 	return str;
 }
 
+void renderBadges(Dromaius *emu) {
+	// Bitmap at 0xD356
+	ImGui::CheckboxFlags("Boulder", (int *)&emu->memory.workram[0x1356], 0x01);
+	ImGui::SameLine();
+	ImGui::CheckboxFlags("Cascade", (int *)&emu->memory.workram[0x1356], 0x02);
+	ImGui::CheckboxFlags("Thunder", (int *)&emu->memory.workram[0x1356], 0x04);
+	ImGui::SameLine();
+	ImGui::CheckboxFlags("Rainbow", (int *)&emu->memory.workram[0x1356], 0x08);
+	ImGui::CheckboxFlags("Soul",    (int *)&emu->memory.workram[0x1356], 0x10);
+	ImGui::SameLine();
+	ImGui::CheckboxFlags("Marsh",   (int *)&emu->memory.workram[0x1356], 0x20);
+	ImGui::CheckboxFlags("Volcano", (int *)&emu->memory.workram[0x1356], 0x40);
+	ImGui::SameLine();
+	ImGui::CheckboxFlags("Earth",   (int *)&emu->memory.workram[0x1356], 0x80);
+}
 
-void gameGUI_pokemon_red(Dromaius *emu) {
+void renderItems(Dromaius *emu) {
 	uint8_t const zero = 0;
 	uint8_t const twofivefour = 254;
 	uint8_t const twofivefive = 255;
+	
+	ImGui::BeginTable("player items", 2, ImGuiTableFlags_Borders);
+	int i;
+	for (i = 0; i < 20; ++i) {
+		// List is 0xFF-terminated
+		if (emu->memory.workram[0x131E + i * 2] == 0xFF) {
+			++i;
+			break;
+		}
 
-	// https://datacrystal.romhacking.net/wiki/Pok%C3%A9mon_Red/Blue:RAM_map
-	auto const playerName = getPokeStringAt(emu, 0xD158, 0x10);
-	auto const rivalName = getPokeStringAt(emu, 0xD34A, 0x10);
-	ImGui::Text("player: %s, rival: %s", playerName.c_str(), rivalName.c_str());
+		ImGui::TableNextColumn();
+		ImGui::PushID(i*2);
+		ImGui::PushItemWidth(-1);
+		ImGui::DragScalar("##item", ImGuiDataType_U8, &emu->memory.workram[0x131E + i * 2], 0.5, &zero, &twofivefour, "Item: %d");
+		ImGui::PopItemWidth();
+		ImGui::PopID();
 
+		ImGui::TableNextColumn();
+		ImGui::PushID(i*2+1);
+		ImGui::PushItemWidth(-1);
+		ImGui::DragScalar("##count", ImGuiDataType_U8, &emu->memory.workram[0x131F + i * 2], 0.5, &zero, &twofivefive, "Count: %d");
+		ImGui::PopItemWidth();
+		ImGui::PopID();
+	}
+	ImGui::EndTable();
+
+	// If the list is not full
+	if (i < 20 && ImGui::Button("Add item")) {
+		// Remove list terminator
+		emu->memory.workram[0x131E + (i-1) * 2] = 0x00;
+
+		// Add a new terminator if this is not the last item
+		if (i < 20) {
+			emu->memory.workram[0x131E + i * 2] = 0xFF;
+		}
+
+		// Update total item count (0xD31D)
+		emu->memory.workram[0x131D] = i;
+	}
+	// ImGui::SameLine();
+	if (i > 1 && ImGui::Button("Remove last")) {
+		emu->memory.workram[0x131E + (i-2) * 2] = 0xFF;
+	}
+}
+
+void renderMons(Dromaius *emu) {
 	// TODO grab Mon info
 	auto monNick = getPokeStringAt(emu, 0xD2B5, 0x10);
 	ImGui::Text("1st mon: %s", monNick.c_str());
 
-	// Items in inventory
-	// auto itemCount = emu->memory.readByte(0xD31D);
-	// for (int i = 0; i < itemCount; ++i) {
-	// 	auto itemNum = emu->memory.readByte(0xD31E + i * 2);
-	// 	auto itemQnt = emu->memory.readByte(0xD31F + i * 2);
-	// 	ImGui::Text("item %d x %d\n", itemNum, itemQnt);
-	// }
+}
+
+void gameGUI_pokemon_red(Dromaius *emu) {
+
+	
+	auto const playerName = getPokeStringAt(emu, 0xD158, 0x10);
+	auto const rivalName = getPokeStringAt(emu, 0xD34A, 0x10);
+
+	// Basic info
+	ImGui::Text("player: %s, rival: %s", playerName.c_str(), rivalName.c_str());
+
+	if (ImGui::CollapsingHeader("Badges")) {
+		renderBadges(emu);
+	}
 
 	if (ImGui::CollapsingHeader("Items (player)")) {
-		// Determine number of items
+		renderItems(emu);
+	}
 
-
-		ImGui::BeginTable("player items", 2, ImGuiTableFlags_Borders);
-		int i;
-		for (i = 0; i < 20; ++i) {
-			// List is 0xFF-terminated
-			if (emu->memory.workram[0x131E + i * 2] == 0xFF) {
-				++i;
-				break;
-			}
-
-			ImGui::TableNextColumn();
-			ImGui::PushID(i*2);
-			ImGui::PushItemWidth(-1);
-			ImGui::DragScalar("##item", ImGuiDataType_U8, &emu->memory.workram[0x131E + i * 2], 0.5, &zero, &twofivefour, "Item: %d");
-			ImGui::PopItemWidth();
-			ImGui::PopID();
-
-			ImGui::TableNextColumn();
-			ImGui::PushID(i*2+1);
-			ImGui::PushItemWidth(-1);
-			ImGui::DragScalar("##count", ImGuiDataType_U8, &emu->memory.workram[0x131F + i * 2], 0.5, &zero, &twofivefive, "Count: %d");
-			ImGui::PopItemWidth();
-			ImGui::PopID();
-		}
-		ImGui::EndTable();
-
-		// If the list is not full
-		if (i < 20 && ImGui::Button("Add item")) {
-			// Remove list terminator
-			emu->memory.workram[0x131E + (i-1) * 2] = 0x00;
-
-			// Add a new terminator if this is not the last item
-			if (i < 20) {
-				emu->memory.workram[0x131E + i * 2] = 0xFF;
-			}
-
-			// Update total item count (0xD31D)
-			emu->memory.workram[0x131D] = i;
-		}
-		// ImGui::SameLine();
-		if (i > 1 && ImGui::Button("Remove last")) {
-			emu->memory.workram[0x131E + (i-2) * 2] = 0xFF;
-		}
-
-
+	if (ImGui::CollapsingHeader("Pokemon (player)")) {
+		renderMons(emu);
 	}
 
 }
